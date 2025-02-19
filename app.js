d0b9ce5f0cbb2ea8ad1b23c998f70a4e22b7dbe9
Adilalee07 <adilaleepanhwar@gmail.com>
2025-02-19 13:07:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// Number1 = 7;
+// Number2 = "10";
+
+// alert (Number1 + Number2);
+// alert (Number1 * Number2);
+// alert (Number1 - Number2);
+// alert (Number1 & Number2);
+
+// var inp = prompt ("Enter Your Name");
+// alert("Welcome " + inp);
+ 
+// var inp = prompt ("Enter Your Profession");
+// alert("You are is " + inp);
+// console.log("Java Assignment");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var Numberone = "30" ;
+var Numbertwo = "20" ;
+
+console.log(":start");
+sum = Numberone + Numbertwo;
+sub = Numberone - Numbertwo;
+multiply = Numberone * Numbertwo;
+divide = Numberone / Numbertwo ;
+
+console.log(sum);
+console.log(sub);
+console.log(multiply);
+console.log(divide);
